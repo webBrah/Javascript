c1edfe9bd4d550ddb21f0563d25f3ec9eaa0f2a5
webBrah <user@Users-MacBook-Air.local>
2020-09-28 22:38:00 -0600

    Sept 28th

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -83,31 +83,62 @@ startGameBtn.addEventListener("click", () => {
 });
 
 // Not related to game
-
-const sumUp = (resultHandler, ...numbers) => {
+const combine = (resultHandler, operation, ...numbers) => {
   const validateNumber = (number) => {
     return isNaN(number) ? 0 : number;
   };
 
   let sum = 0;
   for (const num of numbers) {
-    sum += validateNumber(num);
+    if (operation === "ADD") {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
   }
   resultHandler(sum);
 };
 
-const subtractUp = function() {
-  let sum = 0;
-  for (const num of arguments) { //don' use that
-    sum -= num;
-  }
-  return sum;
+// const subtractUp = function (resultHandler, ...numbers) {
+//   let sum = 0;
+//   for (const num of numbers) {
+//     //don' use that
+//     sum -= num;
+//   }
+//   resultHandler(sum, 'The result after subtracting all numbers is');
+// };
+
+const showResult = (messageText, result) => {
+  alert(messageText + " " + result);
 };
 
-const showResult = (result) => {
-  alert('The result after adding all numbers is: ' + result)
-};
-
-sumUp(showResult, 1, 5, 'fdsa', -3, 6, 10);
-sumUp(showResult, 1, 5, 10, -3, 6, 10, 25, 88);
-console.log(subtractUp(1, 10, 15, 20));
+combine(
+  showResult.bind(this, "The result after adding all numbers is:"),
+  "ADD",
+  1,
+  5,
+  "fdsa",
+  -3,
+  6,
+  10
+);
+combine(
+  showResult.bind(this, "The result after adding all numbers is:"),
+  "ADD",
+  1,
+  5,
+  10,
+  -3,
+  6,
+  10,
+  25,
+  88
+);
+combine(
+  showResult.bind(this, "The result after subtracting all numbers is:"),
+  "SUBTRACT",
+  1,
+  10,
+  15,
+  20
+);
