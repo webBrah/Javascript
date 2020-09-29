70f2b34100a9d11d0274aa81eee18d5936bc3fc4
webBrah <user@Users-MacBook-Air.local>
2020-09-28 21:44:52 -0600

    first commit

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,17 @@
+let name = "Max";
+
+if (name === "Max") {
+  let hobbies = ["sports", "cooking"];
+  console.log(hobbies);
+}
+
+
+function greet() {
+  let age = 30;
+  let name = "Manuel";
+  console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();
